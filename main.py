2d60924b551db87047944c49b8e8c1401bba4fc4
Morvai Barna <morvaibarna@gmail.com>
2023-10-19 15:52:21 +0200

    added better data reader

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import plot_data
 from read_radar_data import X4m200_reader
 import methods as m
 from pymoduleconnector.extras.auto import auto
+from time import sleep
+#TODO: realtime baseband data plot for distance measurement
 
-device_name = auto()[0]
 D = 0.95 # duty cycle
-iterations = 18 # recommended to use 64
+iterations = 300 # recommended to use 64
 dac_min = 949
 dac_max = 1100
 bin_length = 8*1.5e8/23.328e9 # first value is the speed of light, second is the X4 sampling rate
@@ -13,9 +14,9 @@ area_start = 0.4 # start of the sensing area in meters
 area_end = 5 # end of the sensing area in meters
 prf = 15.1875 # pulse repetition frequency (Mhz)
 # FPS = m.calc_max_fps(prf, D, dac_min, dac_max, iterations, pulses_per_step) # calculate the FPS based on the parameters
-FPS = 20 # calculate the FPS based on the parameters
+FPS = 17 # calculate the FPS based on the parameters
 pulses_per_step = m.calc_pulses_per_step(prf, D, dac_min, dac_max, iterations, FPS) # adjust this value to get the desired FPS
-print(pulses_per_step)
+print(f"Pulses per step value: {pulses_per_step}")
 
 
 sample_time = 60 # in seconds
@@ -23,11 +24,15 @@ sample_time = 60 # in seconds
 fast_sample_point = int((area_end - area_start)/bin_length + 2) # number of sample points in the fast time domain
 
 
-reader = X4m200_reader(device_name, FPS, iterations, pulses_per_step, dac_min, dac_max, area_start, area_end, sample_time)
+# device_name = auto()[0]
 
-amp_matrix_path = reader.get_data_matrix()
-print(amp_matrix_path)
+m.countdown()
+
+# reader = X4m200_reader(device_name, FPS, iterations, pulses_per_step, dac_min, dac_max, area_start, area_end, sample_time)
+# amp_matrix_path = reader.get_data_matrix()
+# print(amp_matrix_path)
 # reader.plot_frame(a, b, sample_time)
 
-folder = amp_matrix_path
+# folder = amp_matrix_path
+folder = "133time60s"
 plot_data.perform_fft_analysis(folder, FPS)
